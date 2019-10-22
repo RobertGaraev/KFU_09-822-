2b9d2e46ed4ab12a2c832e313f7ba23565f1537f
RobertGaraev <56035176+RobertGaraev@users.noreply.github.com>
2019-10-22 16:18:53 +0300

    Rename work_with_point.java to Lab2/work_with_point.java

--- Lab2/work_with_point.java
+++ Lab2/work_with_point.java
@@ -12,7 +12,7 @@ public class work_with_point {
             int i = 0;
             while (i<array.size()){
                 if (array.get(i).get_x()==x && array.get(i).get_y()==y){
-                    System.out.print("Òî÷êà óæå çàäàíà  ");
+                    System.out.print("Å‡Ã®Ã·Ä™Å• Ã³Ä‡Äº Ã§Å•Ã¤Å•Ã­Å•  ");
                     System.out.print("x = "+array.get(i).get_x());
                     System.out.println("  y = "+array.get(i).get_y());
                     break;
@@ -22,13 +22,13 @@ public class work_with_point {
             if(i==array.size())array.add(i,new Point(x,y));
         }
     }
-    //ðàññòîÿíèå ìåæäó òî÷êàìè
+    //Ä‘Å•Å„Å„ÅˆÃ®Ë™Ã­ÄÄº Ä›ÄºÄ‡Ã¤Ã³ ÅˆÃ®Ã·Ä™Å•Ä›Ä
      double L(int i, int j){
         double t= Math.pow(array.get(i).get_x()-array.get(j).get_x(),2);
         double p= Math.pow(array.get(i).get_y()-array.get(j).get_y(),2);
         return Math.pow(t+p,0.5);
     }
-    //ïóòü ìåæäó òî÷êàìè
+    //ÄÃ³ÅˆÃ¼ Ä›ÄºÄ‡Ã¤Ã³ ÅˆÃ®Ã·Ä™Å•Ä›Ä
     public double S(int i, int j){
         double s= 0;
         for (int k = i; k < j; k++) {
