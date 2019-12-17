471413f33e2b550f469759dcff1151b3ae8ad175
RobertGaraev <56035176+RobertGaraev@users.noreply.github.com>
2019-12-17 21:45:44 +0300

    Update Main.java

--- Kursovaya/Main.java
+++ Kursovaya/Main.java
@@ -16,57 +16,6 @@ import java.io.IOException;
 
 public class Main extends Application {
 
-//    @Override
-//    public void start(final Stage primaryStage) {
-//
-//        Button button = new Button();
-//        button.setText("Open a New Window");
-//
-//        button.setOnAction(new EventHandler<ActionEvent>() {
-//
-//            @Override
-//            public void handle(ActionEvent event) {
-//
-//                Label secondLabel = new Label("I'm a Label on new Window");
-//
-//                StackPane secondaryLayout = new StackPane();
-//                secondaryLayout.getChildren().add(secondLabel);
-//
-//                Scene secondScene = new Scene(secondaryLayout, 450, 250);
-//
-//                // New window (Stage)
-//                Stage newWindow = new Stage();
-//                newWindow.setTitle("Second Stage");
-//                newWindow.setScene(secondScene);
-//
-//                // Specifies the modality for new window.
-//                newWindow.initModality(Modality.WINDOW_MODAL);
-//
-//                // Specifies the owner Window (parent) for new window
-//                newWindow.initOwner(primaryStage);
-//
-//                // Set position of second window, related to primary window.
-//                newWindow.setX(primaryStage.getX());
-//                newWindow.setY(primaryStage.getY());
-//
-//                newWindow.show();
-//            }
-//        });
-//
-//        StackPane root = new StackPane();
-//        root.getChildren().add(button);
-//
-//        Scene scene = new Scene(root, 450, 250);
-//
-//        primaryStage.setTitle("JavaFX Open a new Window (o7planning.org)");
-//        primaryStage.setScene(scene);
-//        primaryStage.show();
-//    }
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-
     static Parent root;
 
     public static void main(String[] args) {
